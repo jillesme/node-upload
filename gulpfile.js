ffa98f3c2b1f3557b960f8ea476209f9960ac66b
Jilles Soeters <info@jilles.me>
2014-09-09 15:41:10 +0200

    Added Gulp watch, autoprefixer and nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -4,46 +4,77 @@ var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var autoprefixer = require('gulp-autoprefixer');
+var nodemon = require('gulp-nodemon');
 
-/* GULP TASK: delete-images
+/* GULP TASK: clean
  * Deletes all the uploaded images from the /u/ directory
  */
-gulp.task('delete-images', function () {
-  return del(['./public/dist/u/*', './public/src/u/*']);
+gulp.task('clean', function () {
+  del(['./public/dist/u/*', './public/src/u/*']);
 });
 
-/* GULP TASK: lint-JavaScript
+/* GULP TASK: jshint
  * Lints JavaScript files with jshint
  */
-gulp.task('lint-javascript', function () {
+gulp.task('jshint', function () {
   return gulp.src(['./app.js', './public/src/js/upload.js'])
           .pipe(jshint())
           .pipe(jshint.reporter('jshint-stylish'));
 });
 
-/* GULP TASK: compress-javascript
+/* GULP TASK: uglify
  * Uglifies all the (client side) JavaScript files
  */
-gulp.task('compress-javascript', function () {
+gulp.task('uglify', function () {
   return gulp.src('./public/src/js/*.js')
           .pipe(uglify())
           .pipe(gulp.dest('./public/dist/js/'));
 });
 
-/* GULP TASK: compile-sass
+/* GULP TASK: sass
  * Compiles all sass to css
  */
-gulp.task('compile-sass', function () {
+gulp.task('sass', function () {
   return gulp.src('./public/src/scss/*.scss')
+          .pipe(plumber({ errorHandler: onError }))
           .pipe(sass({ outputStyle: 'compressed' }))
+          .pipe(autoprefixer({
+            browsers: [
+            'ios 7',
+            'last 2 Explorer versions',
+            'last 3 Chrome versions',
+            'last 3 Firefox versions'
+            ],
+            cascade: false
+          }))
           .pipe(gulp.dest('./public/dist/css/'));
 });
 
-var tasks = [
-  'delete-images',
-  'lint-javascript',
-  'compress-javascript',
-  'compile-sass'
-  ];
+/* GULP TASK: watch
+ * Watch files for changes and run apropriate tasks
+ */
+gulp.task('watch', function () {
+  gulp.watch('./public/src/js/*.js', ['jshint', 'uglify']);
+  gulp.watch('./public/src/scss/*.scss', ['sass']);
+});
 
-gulp.task('default', tasks);
+/* GULP TASK: run-app
+ * Runs app.js and monitors for changes to restart app.js
+ */
+gulp.task('run-app', function () {
+  return nodemon({
+    script: './app.js',
+    ext: 'js hjs',
+    ignore: ['gulpfile.js', 'upload.js']
+  });
+});
+
+gulp.task('default', ['run-app', 'watch', 'jshint', 'uglify', 'sass']);
+
+function onError (error) {
+  console.log('Error in \'' + error.plugin + '\'');
+  console.log(error.message);
+  this.emit('end');
+}
--- routes/index.js
+++ routes/index.js
@@ -12,78 +12,77 @@ router.get('/', function (req, res) {
 
 /* GET /[imagename] */
 router.get('/*', function (req, res) {
-	// Get the file name from the URL
-	var fileName = req.params[0];
+  // Get the file name from the URL
+  var fileName = req.params[0];
 
-	// Check if it exists synchronously
-	if (fs.existsSync(uploadPath + fileName)) {
-		res.render('image', {
-			title: fileName,
-			image: '/u/' + fileName
-		});
-	} else {
-		res.render('no-image', {
-			imageName: fileName
-		});
-	}
-
+  // Check if it exists synchronously
+  if (fs.existsSync(uploadPath + fileName)) {
+    res.render('image', {
+      title: fileName,
+      image: '/u/' + fileName
+    });
+  } else {
+    res.render('no-image', {
+      imageName: fileName
+    });
+  }
 
 });
 
 
 /* POST upload page. */
 router.post('/upload', function (req, res) {
-	var uploadedFile = req.files.file;
+  var uploadedFile = req.files.file;
 
-	if (isSafe(uploadedFile)) {
-		fs.readFile(uploadedFile.path, function (error, file) {
-			if (!error) {
-				var newName = generateString(6) + '.' + uploadedFile.extension;
-				var newFile = uploadPath + newName;
+  if (isSafe(uploadedFile)) {
+    fs.readFile(uploadedFile.path, function (error, file) {
+      if (!error) {
+        var newName = generateString(6) + '.' + uploadedFile.extension;
+        var newFile = uploadPath + newName;
 
-				fs.writeFile(newFile, file, function (error) {
-					if (!error) {
-						fs.unlink(uploadedFile.path); // delete old file
-						res.send(200, '/' + newName);
-					}
+        fs.writeFile(newFile, file, function (error) {
+          if (!error) {
+            fs.unlink(uploadedFile.path); // delete old file
+            res.send(200, '/' + newName);
+          }
 
-				});
-			} else {
-				res.send(500, 'Couldn\'t read ' + uploadedFile.name);
-			}
+        });
+      } else {
+        res.send(500, 'Couldn\'t read ' + uploadedFile.name);
+      }
 
-		});
-	} else {
-		res.send(403, 'File(type) not allowed.');
-	}
+    });
+  } else {
+    res.send(403, 'File(type) not allowed.');
+  }
 
 });
 
 function isSafe (file) {
-	var MAXFILESIZE = 5000000;
-	var allowedTypes = ['image/jpeg', 'image/png', 'application/pdf', 'text/plain'];
-	var safe = true;
+  var MAXFILESIZE = 5000000;
+  var allowedTypes = ['image/jpeg', 'image/png', 'application/pdf', 'text/plain'];
+  var safe = true;
 
-	if (allowedTypes.indexOf(file.mimetype) === -1 || file.size > MAXFILESIZE) {
-		safe = false;
-	}
+  if (allowedTypes.indexOf(file.mimetype) === -1 || file.size > MAXFILESIZE) {
+    safe = false;
+  }
 
-	return safe;
+  return safe;
 }
 
 function generateString (length) {
   var chars = "abcdefghijklmnopqrstuvwxyz".split('');
-	var len = (length ? length : 6);
-	var randomString = '';
+  var len = (length ? length : 6);
+  var randomString = '';
 
-	for (var i = 0; i < len; i++) {
-		var r = Math.floor(Math.random() * chars.length);
-		// TODO: uppercase or not by math.random();
-		var e = (i % 2 === 0);
-		randomString += e ? chars[r] : chars[r].toUpperCase();
-	}
+  for (var i = 0; i < len; i++) {
+    var r = Math.floor(Math.random() * chars.length);
+    // TODO: uppercase or not by math.random();
+    var e = (i % 2 === 0);
+    randomString += e ? chars[r] : chars[r].toUpperCase();
+  }
 
-	return randomString;
+  return randomString;
 }
 
 module.exports = router;
